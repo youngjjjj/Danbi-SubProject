adddff6099615b3ed92c12f57d21b452d965397e
youngjjjj <youngahn21@naver.com>
2020-11-12 14:40:27 +0900

    ADD board model

--- board/models.py
+++ board/models.py
@@ -1,3 +1,43 @@
-from django.db import models
-
+from django.db import models as m
+from django.conf import settings
 # Create your models here.
+
+
+class TimestampedModel(m.Model):
+    created_at = m.DateTimeField(auto_now_add=True)
+    updated_at = m.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Boards(TimestampedModel):
+    author = m.ForeignKey(settings.AUTH_USER_MODEL, on_delete=m.CASCADE)
+    content = m.TextField()
+
+    def __str__(self):
+        return self.content
+
+    class Meta:
+        db_table = 'board'
+        ordering = ['-id']
+        verbose_name = '게시판'
+
+
+class Comment(TimestampedModel):
+    author = m.ForeignKey(settings.AUTH_USER_MODEL, on_delete=m.CASCADE)
+    board = m.ForeignKey(Boards, on_delete=m.CASCADE)
+    text = m.CharField(max_length=255)
+
+    def __str__(self):
+        return f'{self.author}님이 작성한 댓글 {self.text} 입니다.'
+
+    class Meta:
+        db_table = 'comment'
+        ordering = ['-id']
+        verbose_name = '댓글'
+
+
+
+
+
